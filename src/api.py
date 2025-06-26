86c602ae879860e08fdf71272aedb14e3e8bfdde
Qiuhao Gu <guqh6666@bu.edu>
2025-06-26 09:00:33 -0400

    updating the port from 5000 to 5001

--- src/api.py
+++ src/api.py
@@ -190,5 +190,5 @@ def method_not_allowed(error):
     }), 405
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
 
