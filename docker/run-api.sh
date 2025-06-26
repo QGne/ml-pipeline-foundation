b5cff548fea1616492fda4d9e2a9a4b7747646a3
Qiuhao Gu <guqh6666@bu.edu>
2025-06-26 09:27:19 -0400

    changes made on port 5000 to 5001, swap due to 5000 always be used by other app

--- docker/run-api.sh
+++ docker/run-api.sh
@@ -6,10 +6,10 @@ echo "🚀 Starting ML REST API Server..."
 docker build -f docker/Dockerfile.api -t ml-api:latest .
 
 # Run the API container
-echo "📡 API will be available at http://localhost:5000"
+echo "📡 API will be available at http://localhost:5001"
 echo "🛑 Press Ctrl+C to stop the server"
 
 docker run -it --rm \
-    -p 5000:5000 \
+    -p 5001:5001 \
     --name ml-api-server \
     ml-api:latest
