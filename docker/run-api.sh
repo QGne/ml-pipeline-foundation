48676368f86c8cb76c463af1f25dfeb1bb090303
Qiuhao Gu <guqh6666@bu.edu>
2025-06-30 23:10:28 -0400

    solving for building from req text issue

--- docker/run-api.sh
+++ docker/run-api.sh
@@ -2,6 +2,9 @@
 
 echo "🚀 Starting ML REST API Server..."
 
+#Solving build issue by ensuring the project root location
+cd "$(dirname "$0")/.."
+
 # Build the API Docker image
 docker build -f docker/Dockerfile.api -t ml-api:latest .
 
